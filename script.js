cde944b634e62b24aec2c7f2f5cd98378a774793
matt530bailey <115194527+matt530bailey@users.noreply.github.com>
2022-12-13 10:23:25 -0800

    js/jquerry fixed, time keeper element, dayjs

--- script.js
+++ script.js
@@ -1,65 +1,42 @@
 $(document).ready(() => {
 
-  $('#currentDay').text(dayjs().format('ddd DD, MMM YYYY hh:mm:ss'));
-  $('#currentDay').ready(function () { 
-  }, 1000);
+  $('#currentDay').ready(setInterval(() => { 
+    $('#currentDay').text(dayjs().format('ddd DD, MMM YYYY hh:mm:ss')); 
+      }, 1000));
 
-  $('.saveBtn').on('click', function() {
+  $('.saveBtn').on('click', function () {
     var text = $(this).siblings('.description').val();
     var time = $(this).parent().attr('id');
 
     localStorage.setItem(time, text);
   });
+// timBlock.function
 
   function timeColor() {
-    var currentTime = moment().hour();
-
+    var currentTime = dayjs().format('HH');
+"hour-9" == 09
     $('.time-block').each(function () {
-      var timeBlock = parseInt($(this).attr('id').split('hour')[1]);
-
-      if (timeBlock < currentTime){
+      var timeBlock = parseInt($(this).attr('id').split('hour-')[1]);
+      console.log("timeblock ", timeBlock, "currentTime: ", currentTime);
+        // console.log("this: ", this.children[1]);
+        // console.log("localStore", localStorage.getItem($(this).attr('id')));
+        this.children[1].value = localStorage.getItem($(this).attr('id'))
+      if (timeBlock < currentTime) {
         $(this).removeClass("future");
         $(this).removeClass("present");
         $(this).addClass("past");
       }
-      else if (timeBlock === currentTime) {
-        $(this).removeClass("past");  
-        $(this).removeClass("future");  
+      else if (timeBlock == currentTime) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
         $(this).addClass("present");
       }
       else {
-        $(this).removeClass("past");  
-        $(this).removeClass("present");  
+        $(this).removeClass("past");
+        $(this).removeClass("present");
         $(this).addClass("future");
       }
     })
   }
-
-  // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-  // the code isn't run until the browser has finished rendering all the elements
-  // in the html.
-  $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
-
- 
-}); 
-
+  timeColor();
+});
